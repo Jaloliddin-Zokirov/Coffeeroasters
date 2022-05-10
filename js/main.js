bee53a2ab8e4317a2fa3b5d8aa83a097a9eb762c
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-05-10 16:03:09 +0500

    add new class

--- js/main.js
+++ js/main.js
@@ -8,38 +8,38 @@ elButton.addEventListener("click", function(){
 
 
 var elButtontwo = document.querySelector(".button-two");
-var elContenttwo = document.querySelector(".survery-content__two")
+var elContenttwo = document.querySelector(".box__content-two")
 
 elButtontwo.addEventListener("click", function(){
   elButtontwo.classList.toggle("rotate")
-  elContenttwo.classList.toggle("survery-content__two--open")
+  elContenttwo.classList.toggle("box__content-two--open")
 })
 
 
 var elButtonthere = document.querySelector(".button-there");
-var elContentthere = document.querySelector(".survery-content__there")
+var elContentthere = document.querySelector(".box__content-there")
 
 elButtonthere.addEventListener("click", function(){
   elButtonthere.classList.toggle("rotate")
-  elContentthere.classList.toggle("survery-content__there--open")
+  elContentthere.classList.toggle("box__content-there--open")
 })
 
 
 var elButtontfour = document.querySelector(".button-four");
-var elContentfour = document.querySelector(".survery-content__four")
+var elContentfour = document.querySelector(".box__content-four")
 
 elButtontfour.addEventListener("click", function(){
   elButtontfour.classList.toggle("rotate")
-  elContentfour.classList.toggle("survery-content__four--open")
+  elContentfour.classList.toggle("box__content-four--open")
 })
 
 
 var elButtontfive = document.querySelector(".button-five");
-var elContenttfive = document.querySelector(".survery-content__five")
+var elContenttfive = document.querySelector(".box__content-five")
 
 elButtontfive.addEventListener("click", function(){
   elButtontfive.classList.toggle("rotate")
-  elContenttfive.classList.toggle("survery-content__five--open")
+  elContenttfive.classList.toggle("box__content-five--open")
 })
 
 console.log("Hello world!!!")
